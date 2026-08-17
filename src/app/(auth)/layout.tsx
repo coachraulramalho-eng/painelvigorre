81412a138b09c8ddc4ec4e6213b87e67417ddf71
coachraulramalho-eng <coach.raulramalho@gmail.com>
2026-08-17 18:13:41 -0300

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,12 @@
 import { redirect } from 'next/navigation';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth/auth';
+import { auth } from '@/lib/auth/auth';
 
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
+  const session = await auth();
 
   if (session) {
     redirect('/');
